8069e0b73119bcdfb06c178072bc08448a3c55b0
Johannes Rauh <johannes.rauh@icinga.com>
2025-10-10 09:22:08 +0200

    Add `RequestHook`
    Add a hook that hooks into different states of a request. The
    implementation starts with hooking into after non xhr requests
    are dispatched. This is meant to be extended for more more states
    like before a request gets dispatched.

---
+++ library/Icinga/Application/Hook/RequestHook.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Icinga\Application\Hook;
+
+use Icinga\Application\Hook;
+use Icinga\Web\Request;
+
+abstract class RequestHook extends Hook
+{
+    /**
+     * Triggered after a request has been dispatched
+     *
+     * @param Request $request
+     *
+     * @return void
+     */
+    abstract public function onPostDispatch(Request $request): void;
+
+    /**
+     * Call the onPostDispatch() method of all registered RequestHooks
+     *
+     * @param Request $request
+     *
+     * @return void
+     */
+    final public static function postDispatch(Request $request): void
+    {
+        array_map(
+            fn($hook) => $hook->onPostDispatch($request),
+            static::all('Request')
+        );
+    }
+}
